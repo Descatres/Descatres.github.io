e557ae92a6d9cdc891b8ef531e47758360273d75
João Catré <joaomcatre@hotmail.com>
2024-11-08 21:36:14 +0000

    preparation to add pages animation

--- bookAnimations.js
+++ bookAnimations.js
@@ -7,6 +7,15 @@ export function openBook() {
       main.classList.add("opened");
       rightCover.classList.add("open");
     }, 1000);
+
+    // setTimeout(() => {
+    //   introPage.classList.add("visible"); // Make intro page appear
+    // }, 5500); // Adjust timing to sync with cover
+
+    // setTimeout(() => {
+    //   introPage.classList.add("turn"); // Rotate intro page
+    //   leftPage.classList.add("slide-in"); // Slide in left page
+    // }, 5500);
   });
 }
 
